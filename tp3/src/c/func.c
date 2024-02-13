9dba93fd0947921d68cb6cebf22f9757323d3e53
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2024-02-13 11:43:00 +0100

    Update TP3

--- tp3/src/c/func.c
+++ tp3/src/c/func.c
@@ -3,11 +3,11 @@
  * Created Date: 2023-10-09 08:59:17 pm
  * Author: Mathieu Escouteloup
  * -----
- * Last Modified: 2023-11-29 04:51:14 pm
+ * Last Modified: 2024-02-13 11:42:12 am
  * Modified By: Mathieu Escouteloup
  * -----
  * License: See LICENSE.md
- * Copyright (c) 2023 HerdWare
+ * Copyright (c) 2024 HerdWare
  * -----
  * Description: 
  */
@@ -16,10 +16,9 @@
 
 volatile uint32_t var;
 volatile __attribute__ ((aligned (1))) uint32_t tab[1];
-volatile __attribute__ ((aligned (256))) uint32_t mat0[8][8];
 volatile __attribute__ ((aligned (256))) uint32_t mat1[8][8];
 volatile __attribute__ ((aligned (256))) uint32_t mat2[8][8];
+volatile __attribute__ ((aligned (256))) uint32_t mat3[8][8];
 
 void func () {
-  
 }
